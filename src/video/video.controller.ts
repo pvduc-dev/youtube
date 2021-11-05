28e3218fb7bd125181581a829bd1cdd4184553cc
Pv Duc <pvduc.dev@gmail.com>
2021-11-05 13:53:21 +0700

    Config cache

--- src/video/video.controller.ts
+++ src/video/video.controller.ts
@@ -10,11 +10,4 @@ import { Public } from 'nest-keycloak-connect';
 @Controller('video')
 export class VideoController {
   constructor(private videoService: VideoService) {}
-
-  @Get()
-  @Public()
-  @UseInterceptors(CacheInterceptor)
-  public find() {
-    return this.videoService.findAll();
-  }
 }
