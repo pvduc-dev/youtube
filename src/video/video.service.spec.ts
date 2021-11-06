c1f91530b9d1ddde415ba74a03f259b6c8359c8a
Pv Duc <pvduc.dev@gmail.com>
2021-11-06 16:42:11 +0700

    chore(npm): install axios package

--- src/video/video.service.spec.ts
+++ src/video/video.service.spec.ts
@@ -1,8 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { VideoService } from './video.service';
+import { Model } from 'mongoose';
+import { Video } from '@/video/schemas/video.schema';
 
 describe('VideoService', () => {
   let service: VideoService;
+  let videoModel: Model<Video>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
